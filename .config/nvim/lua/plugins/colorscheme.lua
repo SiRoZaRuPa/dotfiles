2b15664ddb39229c27ef48dd980bb09dc9b23854
Jotaro Emoto <>
2025-12-11 07:27:44 +0000

    [update] 透過設定調整

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
--- ~/.config/nvim/lua/plugins/catppuccin.lua
 return {
-  { "projekt0n/github-nvim-theme", name = "github-theme" },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        -- お好みのオプション
+      })
+      vim.cmd.colorscheme("github_dark_default")
+    end,
+  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    lazy = false,
-    priority = 1000,
+    lazy = true, -- デフォルトで使わないのでlazyに
     opts = {
-      flavour = "latte", -- latte, frappe, macchiato, mocha
+      flavour = "latte",
       integrations = {
         aerial = true,
         alpha = true,
--- .config/nvim/lua/plugins/transparent.lua
+++ .config/nvim/lua/plugins/transparent.lua
@@ -1,16 +1,4 @@
 return {
   "xiyaowong/transparent.nvim",
   lazy = false,
-  opts = {
-    extra_groups = {
-      "NormalFloat", -- フローティングウィンドウ
-      "NvimTreeNormal", -- NvimTree
-      "TelescopeNormal", -- Telescope
-      "WhichKeyFloat",
-    },
-  },
-  config = function(_, opts)
-    require("transparent").setup(opts)
-    require("transparent").clear_prefix("lualine")
-  end,
 }
