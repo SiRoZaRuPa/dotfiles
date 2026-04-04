527f54b7c9e0ef7bc279a29396287f8c77b669f6
SiRoZaRuPa <>
2026-04-04 17:02:07 +0900

    [update]

---
+++ windows/nvim/lua/plugins/editor/hlchunk.lua
@@ -0,0 +1,27 @@
+return {
+	"shellRaining/hlchunk.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("hlchunk").setup({
+			indent = {
+				enable = true,
+				chars = {
+					"│",
+				},
+				style = {
+					"#FF0000",
+					"#FF7F00",
+					"#FFFF00",
+					"#00FF00",
+					"#00FFFF",
+					"#0000FF",
+					"#8B00FF",
+				},
+				delay = 10,
+			},
+			chunk = {
+				enable = false,
+			},
+		})
+	end,
+}
