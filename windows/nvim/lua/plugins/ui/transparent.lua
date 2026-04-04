3502d215c915f904fdcfdb3aafa4b8fa7a9624ba
SiRoZaRuPa <>
2026-04-04 14:33:52 +0900

    [update] Neo-Treeを透過

--- windows/nvim/lua/plugins/ui/transparent.lua
+++ windows/nvim/lua/plugins/ui/transparent.lua
@@ -1,4 +1,13 @@
 return {
-  "xiyaowong/transparent.nvim",
-  lazy = false,
+	"xiyaowong/transparent.nvim",
+	lazy = false,
+	opts = {
+		extra_groups = {
+			"NeoTreeNormal",
+			"NeoTreeNormalNC",
+			-- 必要に応じて以下も追加検討してください
+			"NeoTreeWinSeparator",
+			"NeoTreeEndOfBuffer",
+		},
+	},
 }
