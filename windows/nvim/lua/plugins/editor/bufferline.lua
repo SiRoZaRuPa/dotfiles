72aa1106db0f95d02439386e9d5b9d0c47b71c0f
SiRoZaRuPa <>
2026-04-05 22:37:26 +0900

    [update] hover時の挙動を削除

--- windows/nvim/lua/plugins/editor/bufferline.lua
+++ windows/nvim/lua/plugins/editor/bufferline.lua
@@ -4,16 +4,6 @@ return {
 		options = {
 			-- セパレータ
 			separator_style = "slant",
-			-- ホバー機能の有効化
-			hover = {
-				enabled = true,
-				delay = 10,
-				reveal = { "close" }, -- ホバー時に閉じるボタンを表示
-			},
 		},
 	},
-	init = function()
-		-- マウスホバーイベントを取得するために必須の設定です
-		vim.opt.mousemoveevent = true
-	end,
 }
